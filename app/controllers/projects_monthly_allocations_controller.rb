edddcf4115c0610d30dc63009732b15d00ebe71d
tamura <tamura2004@gmail.com>
2016-08-15 01:21:49 +0900

    Debug

--- app/controllers/projects_monthly_allocations_controller.rb
+++ app/controllers/projects_monthly_allocations_controller.rb
@@ -72,18 +72,18 @@ class ProjectsMonthlyAllocationsController < ApplicationController
         a.cost as cost,
         sum(b.cost) as monthly_allocated_cost,
         sum(d.cost) as member_allocated_cost,
-        sum(case when b.month = '201608' then b.cost end) as 'm201608',
-        sum(case when b.month = '201609' then b.cost end) as 'm201609',
-        sum(case when b.month = '201610' then b.cost end) as 'm201610',
-        sum(case when b.month = '201611' then b.cost end) as 'm201611',
-        sum(case when b.month = '201612' then b.cost end) as 'm201612',
-        sum(case when b.month = '201701' then b.cost end) as 'm201701',
-        sum(case when b.month = '201702' then b.cost end) as 'm201702',
-        sum(case when b.month = '201703' then b.cost end) as 'm201703',
-        sum(case when b.month = '201704' then b.cost end) as 'm201704',
-        sum(case when b.month = '201705' then b.cost end) as 'm201705',
-        sum(case when b.month = '201706' then b.cost end) as 'm201706',
-        sum(case when b.month = '201707' then b.cost end) as 'm201707'
+        sum(case when b.month = '201608' then b.cost end) as m201608,
+        sum(case when b.month = '201609' then b.cost end) as m201609,
+        sum(case when b.month = '201610' then b.cost end) as m201610,
+        sum(case when b.month = '201611' then b.cost end) as m201611,
+        sum(case when b.month = '201612' then b.cost end) as m201612,
+        sum(case when b.month = '201701' then b.cost end) as m201701,
+        sum(case when b.month = '201702' then b.cost end) as m201702,
+        sum(case when b.month = '201703' then b.cost end) as m201703,
+        sum(case when b.month = '201704' then b.cost end) as m201704,
+        sum(case when b.month = '201705' then b.cost end) as m201705,
+        sum(case when b.month = '201706' then b.cost end) as m201706,
+        sum(case when b.month = '201707' then b.cost end) as m201707
       from projects as a
       left join projects_monthly_allocations as b on a.id = b.project_id
       left join projects_members as c on a.id = c.project_id
