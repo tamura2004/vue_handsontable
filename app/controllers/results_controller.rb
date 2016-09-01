07a2f05e3c203e484abe91da889f6a2f9a0d57a1
tamura <tamura2004@gmail.com>
2016-09-01 13:12:03 +0000

    fire

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,7 +1,9 @@
 class ResultsController < ApplicationController
 
-  ATTRIBUTES = %w(month group_number group_name outsource_code company_name member_number member_name project_number project_name proc_number proc_name cont_number cont_name system_number cost cost_minute)
-  LABELS = %w(稼働年月 部署コード 部署名 外注区分 会社名 社員番号 社員名 案件番号 案件名 工程コード 工程名称 契約種別コード 契約種別名称 システムナンバー 工数 工数分)
+  ATTRIBUTES = %w(member_number member_name project_number project_name cost)
+  LABELS = %w(社員番号 社員名 案件番号 案件名 工数)
+  # ATTRIBUTES = %w(month group_number group_name outsource_code company_name member_number member_name project_number project_name proc_number proc_name cont_number cont_name system_number cost cost_minute)
+  # LABELS = %w(稼働年月 部署コード 部署名 外注区分 会社名 社員番号 社員名 案件番号 案件名 工程コード 工程名称 契約種別コード 契約種別名称 システムナンバー 工数 工数分)
   HEADERS = ATTRIBUTES.inject({}){|memo,a| memo.merge a.to_sym => a}
 
   def index
